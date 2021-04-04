371449cbd589b5864fef383af8c08b91e7350fe1
zhouyunchong <943174533@qq.com>
2021-04-04 09:21:13 +0800

    app壳工程

--- business/acomponent/src/main/java/com/example/acomponent/MainActivityFromA.java
+++ business/acomponent/src/main/java/com/example/acomponent/MainActivityFromA.java
@@ -9,6 +9,6 @@ public class MainActivityFromA extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_from);
+        setContentView(R.layout.activity_main_from_a);
     }
 }
